b00c0f15157603b01e67ac990d77da9d8c5337a3
gebeyaw <gebeelek143@gmail.com>
2024-12-06 22:46:00 +0300

    add optional query parameters

--- queryParams.py
+++ queryParams.py
@@ -1,10 +1,12 @@
+"""Optional parameters"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
+@app.get("/goods/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
