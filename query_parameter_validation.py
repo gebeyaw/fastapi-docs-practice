dd997d06527252abbeb2608b08c2343d2fe5804d
gebeyaw <gebeelek143@gmail.com>
2024-12-10 00:31:44 +0300

    Include min_length query parameter validation

--- query_parameter_validation.py
+++ query_parameter_validation.py
@@ -4,7 +4,7 @@ from typing import Annotated
 app = FastAPI()
 
 @app.get("/items/")
-async def create_item(q: Annotated[str | None, Query(max_length=10)] = None):
+async def create_item(q: Annotated[str | None, Query(min_length=3, max_length=10)] = None):
     results=  {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q":q})
